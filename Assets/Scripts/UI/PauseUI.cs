7c7f57cbea300c67f164574150b10813a7d58a2c
Kou <kurobakouya@gmail.com>
2024-04-18 23:28:07 +0800

    v.0.0.32 [Add] UI Assets - Added UI assets - PauseUI WIP

---
+++ Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : UIBase
+{
+    public override void Toggle()
+    {
+        base.Toggle();
+    }
+}
